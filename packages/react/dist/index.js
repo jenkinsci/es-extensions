9db32d6186e2a669426c6fa4649fa82e1e44d476
Ivan Meredith <ivan@ivan.net.nz>
2018-10-10 11:30:10 +1300

    Add build script

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { extensionStore } from '@imeredith/es-extensions-api';
+import { ExtensionStore } from '@imeredith/es-extensions-api';
 export class RenderExtensions extends React.Component {
     render() {
-        return extensionStore
-            .get(this.props.extensionPointId)
-            .map((extension, index) => React.createElement("div", { key: index.toString(), ref: container => container && extension(Object.assign({}, this.props.context, { container })), className: "extension" }));
+        return ExtensionStore
+            .getExtensions(this.props.extensionPointId)
+            .map((extension, index) => React.createElement("div", { key: `${this.props.keyPrefix}${index.toString()}`, ref: container => container && extension(Object.assign({}, this.props.context, { container })), className: this.props.containerClassName }));
     }
 }
