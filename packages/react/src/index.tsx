515a64cba0ba9c136356a9b24b7d89068dc08e9b
Ivan Meredith <ivan@ivan.net.nz>
2018-10-11 11:24:15 +1300

    Add defualt render

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -3,17 +3,22 @@ import { ExtensionStore, RenderContext } from '@imeredith/es-extensions-api';
 
 interface Props {
     extensionPointId: string
-    context: any
-    containerClassName: string
-    keyPrefix: string
+    context?: any
+    containerClassName?: string
+    keyPrefix?: string
+ 
 }
 export class RenderExtensions extends React.Component<Props> {
     render() {
-        return ExtensionStore
-            .getExtensions(this.props.extensionPointId)
-            .map((extension: any, index: number) => 
-                <div key={`${this.props.keyPrefix}${index.toString()}`} 
-                    ref={container => container && extension({...this.props.context, container})}
-                    className={this.props.containerClassName} />)
+        const exts =  ExtensionStore.getExtensions(this.props.extensionPointId);
+        if(!exts || exts.length == 0) {
+            return <div className={this.props.containerClassName}>
+                {this.props.children}
+            </div>
+        }
+        return exts.map((extension: any, index: number) => 
+            <div key={`${this.props.keyPrefix}${index.toString()}`} 
+                ref={container => container && extension({...this.props.context, container})}
+                className={this.props.containerClassName} />)
     }
 }
